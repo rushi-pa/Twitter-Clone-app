faee7a2f8f70ed578e5be6a370bff02a1a54ac54
rushi-pa <rushipatel102001@gmail.com>
2021-09-04 21:45:56 -0400

    tweet unlike completed

--- Twitter-app/ViewModel/TweetActionViewModel.swift
+++ Twitter-app/ViewModel/TweetActionViewModel.swift
@@ -12,19 +12,37 @@ class TweetActionViewModel:ObservableObject {
     @Published var didLike = false
     init(tweet :Tweet) {
         self.tweet = tweet
+        ifLiked();
     }
     func likeTweet(){
-        guard let currentUid = Auth.auth().currentUser?.uid else {
+        guard let uid = Auth.auth().currentUser?.uid else {
             return
         }
         COLLECTION_Tweet.document(tweet.id).updateData(["likes" : tweet.likes + 1]);
-        COLLECTION_Tweet.document(tweet.id).collection("tweet-likes").document(currentUid).setData([:]) { _ in
-            COLLECTION_Tweet.document(currentUid).collection("user-likes").document(self.tweet.id).setData([:]) { _ in
+        COLLECTION_Tweet.document(tweet.id).collection("tweet-likes").document(uid).setData([:]) { _ in
+            COLLECTION_USERS.document(uid).collection("user-likes").document(self.tweet.id).setData([:]) { _ in
                 self.didLike = true
             }
         }
     }
     func unlikeTweet(){
-        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        COLLECTION_Tweet.document(tweet.id).updateData(["likes" : tweet.likes - 1]);
+        COLLECTION_Tweet.document(tweet.id).collection("tweet-likes").document(uid).delete { _ in
+            COLLECTION_USERS.document(uid).collection("user-likes").document(self.tweet.id).delete { _ in
+                self.didLike = false
+            }
+        }
+    }
+    func ifLiked(){
+        guard let currentUid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        COLLECTION_USERS.document(currentUid).collection("user-likes").document(tweet.id).getDocument { snapshot, _ in
+           guard let didLike = snapshot?.exists else {return}
+            self.didLike = didLike;
+        }
     }
 }
